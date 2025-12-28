c944f4175d146b4f09985d125d9c3e63cc490a51
Shlok-Dubey <dubeyshlok12@gmail.com>
2025-12-28 21:44:40 +0530

    worte some notes

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,7 @@
+// to initialsed the project we should use "cargo init" cmd this will make "cargo.toml" and "main.rs" files
+/* to initialsed library use "cargo init --lib", you will be able to use this in diffrent projects
+ but won't be able to excute it */ 
+
 fn main() {
     println!("Hello, world!");
 }
