a170498537148ce1b81b3f50b35ae5664fdfb723
Shlok-Dubey <dubeyshlok12@gmail.com>
2025-12-28 23:46:24 +0530

    done notes of hello_world

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,6 +1,11 @@
 // to initialsed the project we should use "cargo init" cmd this will make "cargo.toml" and "main.rs" files
 /* to initialsed library use "cargo init --lib", you will be able to use this in diffrent projects
  but won't be able to excute it */ 
+// to run "cargo run"
+// to build "cargo build" --> this will create target/debug/file_name (this is development build)
+/* to production build "cargo build --release" --> this will create target/release/file_name (more
+ optimised then development) */
+
 
 fn main() {
     println!("Hello, world!");
